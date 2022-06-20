439a1bc8142d7761d5a4d4d6a47df89fd63239e4
Sakkaya <94840303+bn-moonlight@users.noreply.github.com>
2022-06-20 11:11:16 +0300

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -25,3 +25,7 @@ int	ft_memcmp(const void *dst, const void *src, size_t n)
 	}
 	return (0);
 }
+
+// isaret edilen 2 bellek bolgesinin de ayni olup olmadigini karsilastirir.
+// strcmp --> (string comperson) --> stringleri karsilastirir.
+// memcmp --> (memory comperson)--> bellek bolgelerini karsilastirir.
