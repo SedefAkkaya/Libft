dbd7ff626ad5a673e2523fc5167b7668d2a288af
Sakkaya <94840303+bn-moonlight@users.noreply.github.com>
2022-06-21 11:51:07 +0300

    oaa

--- ft_memset.c
+++ ft_memset.c
@@ -24,3 +24,20 @@ void	*ft_memset(void *src, int c, size_t n)
 	}
 	return (src);
 }
+
+/*
+int main (int args, char **argv)
+{
+	int arr1[] = {3, 4};
+	
+	ft_memset(arr1, 0, 2 * sizeof(int));
+	printf("arr1 = %d, %d", arr1[0], arr1[1]);
+}
+*/
+
+// ft_memset(neyin icini, neyle, ne kadar) dolduruyoruz.
+// Bu kod, normaldeki memsettir.
+// Burada icini 0 ile doldurdugumuzda bir sıkıntı cikmiyor. 
+// Fakat icerisini 0'dan farkli bir sey ile dosdurursak alakasiz sonuclar elde ederiz.
+// Sebebi ise 22. satirda pointeri char'a donustirmusuz. Oysaki icine int degeri atiyoruz.
+// Eger 0'dan farkli bir degerler ile de dolmasini istiyorsak 22. satirda "unsigned char" degil, "int" yapmamiz lazim.
